99989804216bee11f399e2cd02a80de5e138e52e
Raul <jimenezraul1981@gmail.com>
2025-01-29 12:43:07 -0500

    Add initial dashboard layout with Tailwind CSS styling and navbar component

---
+++ src/app/layout/dashboard-layout/dashboard-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from "../../shared/navbar/navbar.component";
+
+@Component({
+  selector: 'app-dashboard-layout',
+  imports: [NavbarComponent],
+  templateUrl: './dashboard-layout.component.html',
+  styleUrl: './dashboard-layout.component.css'
+})
+export class DashboardLayoutComponent {
+
+}
---
+++ src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
+  imports:[CommonModule]
+})
+export class NavbarComponent {
+  isMobileMenuOpen = false;
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+}

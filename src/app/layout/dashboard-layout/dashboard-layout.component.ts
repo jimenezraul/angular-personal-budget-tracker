5072bccecad04b9f3029800cca6eed7b70d8b2b7
Raul <jimenezraul1981@gmail.com>
2025-01-29 14:20:01 -0500

    Add dock menu component and update navbar styling

--- src/app/layout/dashboard-layout/dashboard-layout.component.ts
+++ src/app/layout/dashboard-layout/dashboard-layout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../shared/navbar/navbar.component";
+import { DockMenuComponent } from "../../shared/dock-menu/dock-menu.component";
 
 @Component({
   selector: 'app-dashboard-layout',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, DockMenuComponent],
   templateUrl: './dashboard-layout.component.html',
   styleUrl: './dashboard-layout.component.css'
 })
---
+++ src/app/shared/dock-menu/dock-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dock-menu',
+  imports: [],
+  templateUrl: './dock-menu.component.html',
+  styleUrl: './dock-menu.component.css'
+})
+export class DockMenuComponent {
+
+}
